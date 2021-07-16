2cfe9f096eba54fb6f4c56ce14b303522f2ef4e8
VishwajeetMishra21 <vishwajeetmishracode21@gmail.com>
2021-07-16 12:48:57 +0530

    vi

--- app/src/main/java/com/example/memes/MainActivity.kt
+++ app/src/main/java/com/example/memes/MainActivity.kt
@@ -19,14 +19,14 @@ import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var Image : ImageView
-    lateinit var next : Button
-    lateinit var share : Button
-    var currentImage : String? = null
-    lateinit var big_theme : MaterialButtonToggleGroup
-    lateinit var dark : Button
-    lateinit var light : Button
-    lateinit var progressBar : ProgressBar
+    lateinit var Image: ImageView
+    lateinit var next: Button
+    lateinit var share: Button
+    var currentImage: String? = null
+    lateinit var big_theme: MaterialButtonToggleGroup
+    lateinit var dark: Button
+    lateinit var light: Button
+    lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,34 +40,39 @@ class MainActivity : AppCompatActivity() {
         light = findViewById(R.id.light)
         progressBar = findViewById(R.id.progressBar)
 
+        //It provide the meme
         search()
 
+
+        //When click on next button for next meme
         next.setOnClickListener {
             search()
         }
 
-            big_theme.addOnButtonCheckedListener(object : MaterialButtonToggleGroup.OnButtonCheckedListener{
-                override fun onButtonChecked(
-                    group: MaterialButtonToggleGroup?,
-                    checkedId: Int,
-                    isChecked: Boolean
-                ) {
-                    val theme = when(checkedId) {
-                        R.id.dark -> AppCompatDelegate.MODE_NIGHT_YES
-                        else -> AppCompatDelegate.MODE_NIGHT_NO
-                    }
-                    AppCompatDelegate.setDefaultNightMode(theme)
+        //dark theme and light theme
+        big_theme.addOnButtonCheckedListener(object :
+            MaterialButtonToggleGroup.OnButtonCheckedListener {
+            override fun onButtonChecked(
+                group: MaterialButtonToggleGroup?,
+                checkedId: Int,
+                isChecked: Boolean
+            ) {
+                val theme = when (checkedId) {
+                    R.id.dark -> AppCompatDelegate.MODE_NIGHT_YES
+                    else -> AppCompatDelegate.MODE_NIGHT_NO
                 }
+                AppCompatDelegate.setDefaultNightMode(theme)
+            }
 
-            })
+        })
 
 
-
+         //Sharing the Meme
         share.setOnClickListener {
             val intent = Intent(Intent.ACTION_SEND)
             intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT,"Share this please")
-            val chooser = Intent.createChooser(intent,"Share $currentImage")
+            intent.putExtra(Intent.EXTRA_TEXT, "Share this please")
+            val chooser = Intent.createChooser(intent, "Share $currentImage")
             startActivity(chooser)
         }
 
@@ -78,28 +83,28 @@ class MainActivity : AppCompatActivity() {
         val queue = Volley.newRequestQueue(this@MainActivity)
         val url = "https://meme-api.herokuapp.com/gimme"
 
-        val jsonObjectRequest = object : JsonObjectRequest(Request.Method.GET,url,null, Response.Listener {
-            try{
+        val jsonObjectRequest =
+            object : JsonObjectRequest(Request.Method.GET, url, null, Response.Listener {
+                try {
 
-                println("Response are $it")
-                progressBar.visibility = View.GONE
-                currentImage = it.getString("url")
-                Picasso.get().load(currentImage).into(Image)
-            }catch (e:JSONException){
-                Toast.makeText(this,"Error Josn",Toast.LENGTH_SHORT).show()
-            }
+                    println("Response are $it")
+                    progressBar.visibility = View.GONE
+                    currentImage = it.getString("url")
+                    Picasso.get().load(currentImage).into(Image)
+                } catch (e: JSONException) {
+                    Toast.makeText(this, "Error Josn", Toast.LENGTH_SHORT).show()
+                }
 
-        },
-        Response.ErrorListener {
-                println("Error are occ $it")
-        })
-        {
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String,String>()
-                headers["User-Agent"] = "Mozilla/5.0"
-                return headers
+            },
+                Response.ErrorListener {
+                    println("Error are occ $it")
+                }) {
+                override fun getHeaders(): MutableMap<String, String> {
+                    val headers = HashMap<String, String>()
+                    headers["User-Agent"] = "Mozilla/5.0"
+                    return headers
+                }
             }
-        }
         queue.add(jsonObjectRequest)
 
     }
